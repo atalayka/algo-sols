6f5dba725f809125644efa16493216f5ba722e64
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-08 14:49:32 +0300

    Sum of the first nth term of Series
    https://www.codewars.com/kata/555eded1ad94b00403000071/train/csharp

---
+++ algo1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+
+        public class Algorithm
+        {
+            public static string seriesSum(int n)
+            {
+                return (from i in Enumerable.Range(0, n) select 1.0 / (3 * i + 1)).Sum().ToString("0.00");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine(Algorithm.seriesSum(5));
+
+
+
+
+            ////////////////////////////////////////////////////
+        }
+
+    }
+}
