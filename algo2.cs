60db0c81a227dd31373e5c951359b3b7a66d13c2
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-09 14:42:53 +0300

    Remove the minimum
    https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/csharp

---
+++ algo2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        public class Algorithm
+        {
+            public static List<int> RemoveSmallest(List<int> numbers)
+            {
+                List<int> numbersCopy = numbers.ToList();
+
+                if (numbersCopy.Count == 0)
+                {
+                    return numbersCopy;
+                }
+
+                int smallestValue = numbersCopy.Min();
+                numbersCopy.Remove(smallestValue);
+                return numbersCopy;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //List<int> testList = new List<int> { };
+            List<int> testList = new List<int> { 7, 3, 8, 2, 1, 10, 9, 5, 4, 6 };
+            Console.WriteLine(string.Join(",", Algorithm.RemoveSmallest(testList)));
+        }
+    }
+}
