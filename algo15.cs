8b047ffb7a907c9361aa61cd3173d2b6b2f70e43
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 12:58:16 +0300

    Beginner Series #1 School Paperwork
    https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd

---
+++ algo15.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Paper
+{
+  public static int Paperwork(int n, int m)
+  {
+    int BPage = n * m;
+
+    if (n < 0 || m < 0)
+    {
+      BPage = 0;
+    }
+
+    return BPage;
+
+  }
+}
