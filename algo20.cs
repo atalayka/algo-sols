1edbaeb6fe64c1f2f3825338678073051990f328
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 13:03:19 +0300

    Sum of two lowest positive integers
    https://www.codewars.com/kata/558fc85d8fd1938afb000014

---
+++ algo20.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+  class Digitizer
+  {
+    public static long[] Digitize(long n)
+    {
+       string numberStr = n.ToString();
+            long[] result = new long[numberStr.Length];
+            for (int i = 0; i < numberStr.Length; i++)
+            {
+                result[i] = long.Parse(numberStr[i].ToString());
+            }
+            Array.Reverse(result);
+            return result;
+    }
+  }
+}
