34534403789b7ed18742139eb415303d2d0aef64
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 13:03:59 +0300

    Reversed sequence
    https://www.codewars.com/kata/5a00e05cc374cb34d100000d

---
+++ algo21.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int[] ReverseSeq(int n)
+  {
+    
+            int[] numbers = new int[n];
+            int Counter = 0;
+
+            for (int i = n; i > 0; i--)
+            {
+                numbers[Counter] = i;
+                Counter++;
+            }
+
+            return numbers;
+  }
+}
