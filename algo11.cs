5cfe86ffbf10082b08d92b5f5d31990f55f77fa9
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 12:53:50 +0300

    Reverse words
    https://www.codewars.com/kata/5259b20d6021e9e14c0010d4

---
+++ algo11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string ReverseWords(string str)
+  {
+            string str1 = "";
+            string str2 = "";
+            int sayac = 0;
+            string[] SplittedString = str.Split(" ");
+            foreach (var item in SplittedString)
+            {
+
+                str1 = item.ToString();
+
+                if (sayac > 0) { str1 += " "; }
+                sayac++;
+
+                for (int i = str1.Length - 1; i >= 0; i--)
+                {
+                    str2 += str1[i];
+                }
+
+            }
+
+            return str2;
+
+  }
+}
