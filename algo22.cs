cb33ca5ff497a9bdeb3beec31435bbd57ae08d6e
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 13:04:56 +0300

    Replace every nth
    https://www.codewars.com/kata/57fcaed83206fb15fd00027a

---
+++ algo22.cs
@@ -0,0 +1,33 @@
+﻿public class Kata
+{
+  public static string ReplaceNth(string text, int n, char oldValue, char newValue)
+  {
+    // Check if the input parameters are valid.
+    if (n <= 0 || n > text.Length)
+    {
+      return text;
+    }
+
+    // Create a new string builder to store the replaced text.
+    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+    // Iterate over the text string, keeping track of the number of times oldValue has been encountered.
+    int count = 0;
+    foreach (char c in text)
+    {
+      if (c == oldValue && ++count % n == 0)
+      {
+        // If the current index is a multiple of n and the current character is oldValue, append the newValue character to the StringBuilder object.
+        sb.Append(newValue);
+      }
+      else
+      {
+        // Otherwise, append the current character to the StringBuilder object.
+        sb.Append(c);
+      }
+    }
+
+    // Return the replaced text.
+    return sb.ToString();
+  }
+}
