65d774ec6d1242f9b3389e096c0c6ee17bd10455
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 13:05:40 +0300

    Maximum Multiple
    https://www.codewars.com/kata/5aba780a6a176b029800041c

---
+++ algo23.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int MaxMultiply(int divisor, int bound)
+    {
+        var largestN = Enumerable.Range(1, bound)
+            .Where(n => n % divisor == 0)
+            .LastOrDefault();
+
+        return largestN;
+    }
+}
