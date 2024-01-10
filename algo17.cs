3f365a4339c03b1c9bc8acc2058dfe07c5fc40d5
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 13:00:06 +0300

    The Feast of Many Beasts
    https://www.codewars.com/kata/5aa736a455f906981800360d

---
+++ algo17.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+    public static bool Feast(string beast, string dish)
+    {
+        char[] chars = beast.ToCharArray();
+        char[] char1 = dish.ToCharArray();
+
+        // Check if the first and last letters of the two strings are the same.
+        return chars[0] == char1[0] && chars[^1] == char1[^1];
+    }
+}
