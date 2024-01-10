a829409507f51c7cec81fc2d96a8a88592ba8f30
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 12:54:54 +0300

    Sum Arrays
    https://www.codewars.com/kata/53dc54212259ed3d4f00071c

---
+++ algo12.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static double SumArray(double[] array)
+  {
+    double sum = 0;
+  foreach (double number in array)
+  {
+    sum += number;
+  }
+  return sum;
+  }
+}
