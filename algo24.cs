138ab2c211f71a7015694fda1e5bff885b9d5379
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 13:06:35 +0300

    Hero's root
    https://www.codewars.com/kata/55efecb8680f47654c000095

---
+++ algo24.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class IntSqRoot 
+{
+	public static long IntRac(long n, long guess)  
+	{
+		long x = guess;
+		long prevX;
+		int count = 0;
+
+		do {
+			prevX = x;
+			x = (x + n / x) / 2;
+			count++;
+		} while (Math.Abs(x - prevX) >= 1);
+
+		return count;
+	}
+}
