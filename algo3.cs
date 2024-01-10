c70310a03430a07893eb30310785782495d9d8d2
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 12:31:26 +0300

    Vowel Count
    https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/csharp

---
+++ algo3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        public class Algorithm
+        {
+            public static int vowelCount(string str){
+        
+             int count = str.Where(c => c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u').Count();
+             return count;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string x = "abracadabra";
+            Console.WriteLine(Algorithm.vowelCount(x));
+        }
+    }
+}
