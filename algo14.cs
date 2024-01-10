e4c8bcca3bf0a40a7d9cedc334996c1ca898ed6c
atalai kabakci <58311022+fullmetaljacked@users.noreply.github.com>
2024-01-10 12:57:23 +0300

    Find the smallest integer in the array
    https://www.codewars.com/kata/55a2d7ebe362935a210000b2

---
+++ algo14.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+    public static int FindSmallestInt(int[] args)
+    {
+        int smallest = args[0];
+        foreach (int number in args)
+        {
+            if (number < smallest)
+            {
+                smallest = number;
+            }
+        }
+        return smallest;
+    }
+}
