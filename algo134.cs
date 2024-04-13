a8b06aaf92ee3f29fd6ff6ec2fce7e2aeb93aadf
atalayk <58311022+atalayka@users.noreply.github.com>
2024-04-13 13:12:41 +0300

    String ends with?
    https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/csharp

---
+++ algo134.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
